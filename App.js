8be96c1581529b26d4996a522be1780c51c75505
EinzigartigR <97392513+EinzigartigR@users.noreply.github.com>
2024-08-26 09:50:36 +0530

    Update App.js
    till 26th morning

--- App.js
+++ App.js
@@ -3,14 +3,29 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import NameList from './components/NameList';
 import NameTable from './components/NameTable';
+import { useState } from 'react';
 
 function App() {
-  let arr=["Anna","Raji","Amar","Reni"]
+  let [arr,setarr]=useState(["Anna","Raji","Amar","Reni"])
+
+  const addnewname=(name)=>{
+    setarr([...arr,name])
+  }
+
+  const deleteoldname=(name)=>{
+    var newarr = arr.filter((nm)=> nm!==name);
+    setarr(newarr)
+  }
+
+  const updateintoname=(name)=>{
+    
+  }
   return (
     <div>
       <Header></Header>
+      Search : <input type='text' name='search' id='search' value="search"></input>
       <div className='mydiv'>
-      <NameList namearr={arr}></NameList>
+      <NameList namearr={arr} insertnewname={addnewname}  removeoldname={deleteoldname} modifyintoname={updateintoname}></NameList>
       </div>
       <div className='mydiv'>
       <NameTable namearr={arr}></NameTable>
