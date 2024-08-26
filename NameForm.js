770537ee364509c8481d60eaf7a3fac011bab648
EinzigartigR <97392513+EinzigartigR@users.noreply.github.com>
2024-08-26 09:50:14 +0530

    Update NameForm.js
    till 26th morning

--- NameForm.js
+++ NameForm.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function NameForm() {
+export default function (props) {
+  let [name,setname]=useState("");
+  useEffect(()=>{
+    console.log(name)
+  },[name])
+
+  const handleChange=(event)=>{
+    setname(event.target.value)
+  }
+
+  const addname= () => {
+    console.log("in name form add");
+    if(name === "" || name.trim().length==0){
+      alert("name cannot be empty")
+      return
+    }
+    props.addnewname(name)
+    setname=("")
+  }
+
+  const deletename= () => {
+    console.log("in name form delete");
+    props.deleteoldname(name)
+    setname("")
+  }
+
+  const updatename= () => {
+    console.log("in name form add");
+    props.updateintoname(name)
+  }
+
   return (
-    <div>NameForm</div>
+    <div>
+      <form>
+        Name : <input type='text' name='name' id='name'
+        value={name}
+      onChange={handleChange}></input><br></br>
+        <button type='button' id='btn' value="add" onClick={addname}>Add Name</button>
+        <button type='button' id='btn' value="delete" onClick={deletename}>Delete Name</button>
+        <button type='button' id='btn' value="update" onClick={updatename}>Update Name</button>
+      </form>
+    </div>
   )
 }
