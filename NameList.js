ec5b3cc0d103424270c4320aa0c5f86e8cbc60cb
EinzigartigR <97392513+EinzigartigR@users.noreply.github.com>
2024-08-26 09:49:41 +0530

    Update NameList.js
    till 26th morning

--- NameList.js
+++ NameList.js
@@ -2,12 +2,25 @@ import React from 'react'
 import NameForm from './NameForm';
 
 export default function NameList(props) {
+
+  const insertname=(name)=>{
+  props.insertnewname(name)
+  }
+
+  const removename = (name) =>{
+    props.removeoldname(name)
+  }
+
+  const modifyname =(name)=>{
+    props.modifyintoname(name)
+  }
+
   return (
     <div>
         <ul>
             {props.namearr.map((nm,index,arr)=><li key={index}>{index+1}. {nm}</li>)}
         </ul>
-        <NameForm></NameForm>
+        <NameForm addnewname = {insertname} deleteoldname={removename} updateintoname={modifyname}></NameForm>
     </div>
   )
 }
